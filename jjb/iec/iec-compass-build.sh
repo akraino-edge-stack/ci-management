8f6374d0df161f5762d515546116f778ba49544a
Jingzhao.Ni <Jingzhao.Ni@arm.com>
2019-05-27 20:31:16 +0800

    Specific the python-pip to default version
    The CI platform can not deployed properly since the wrong pip
    version. In this patch, the pip version was specified to default
    version for fixing it.
    
    Signed-off-by: Jingzhao.Ni <Jingzhao.Ni@arm.com>
    Change-Id: Ia86e5a8b1c75e6367d044e94e356f7d1a855418e

--- jjb/iec/iec-compass-build.sh
+++ jjb/iec/iec-compass-build.sh
@@ -14,6 +14,8 @@ sudo docker rm -f `docker ps | grep compass | cut -f1 -d' '` || true
 
 curl -s http://people.linaro.org/~yibo.cai/compass/compass4nfv-arm64-fixup.sh | bash || true
 
+sed -i "s/sudo pip install pyyaml/python -m pip install pyyaml --user/g" build.sh
+
 ./build.sh
 
 exit 0
