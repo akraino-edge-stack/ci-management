4cf1500a811b99a043aea66f3802752c9fabc42a
Jingzhao.Ni <Jingzhao.Ni@arm.com>
2020-03-05 18:03:22 +0800

    Fix installation bug on Ubuntu 18.04
    When deploying VMs on Ubuntu 18.04, the compass can not resolve the
    host ip address correctly. It will cause the compass4nfv deploying
    failed.
    In this patch, it will use the "ip addr" to replace the "ifconfig"
    command in configuration file.
    
    Signed-off-by: Jingzhao.Ni <Jingzhao.Ni@arm.com>
    Change-Id: I86faffee4473ed2f6f36594a2b14be44f7b77191

--- jjb/iec/iec-compass-build.sh
+++ jjb/iec/iec-compass-build.sh
@@ -26,6 +26,13 @@ checkout_arm64(){
   VERSION="dcc6d07"
   git checkout ${VERSION}
   sed -i 's:opnfv/:cyb70289/:' build/build-aarch64.yaml
+
+  # The different version of ifconfig command will result in compass building failed
+  # on different Ubuntu system. Use the "ip addr" to replace the ifconfig may be a
+  # good way for getting host ip address. So, there are some modifications in
+  # configuration file.
+  sed -i "s/^    host_ip.*$/    host_ip=\`ip addr show dev \$external_nic \| grep \'inet \' \| cut -d\/ -f1 \| awk \'\{ print \$2\}\'\`/g" \
+         util/docker-compose/roles/compass/tasks/main.yml
 }
 
 check_env
@@ -46,8 +53,6 @@ mkdir -p $COMPASS_WORK_DIR
 ln -s $COMPASS_WORK_DIR work
 
 sudo docker rm -f `sudo docker ps | grep compass | cut -f1 -d' '` || true
-
-curl -sL http://people.linaro.org/~yibo.cai/compass/compass4nfv-arm64-fixup.sh | bash || true
 
 ./build.sh
 
